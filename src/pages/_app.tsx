a96f70800e6428456f642b16b99d13b7177e7530
mohit <connectwithlastdev@gmail.com>
2024-04-19 14:57:12 +0530

    finish header ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import Header from "@/components/ui/Header";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (<>
+    <Header/>
+    <Component {...pageProps} />;
+  </>
+  )
 }
