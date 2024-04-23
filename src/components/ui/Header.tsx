225e3651f2f85eeeb5a7e71cb6390025b1d322db
mohit <connectwithlastdev@gmail.com>
2024-04-23 12:26:23 +0530

    setup dropdown and auth checkin

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,28 +1,14 @@
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil';
-import { useSession, signIn} from 'next-auth/react';
+import { useSession, signIn } from 'next-auth/react';
 import { authState } from '@/state/authState';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem } from '@radix-ui/react-dropdown-menu';
+import { DropdownMenuDemo } from './DropdownMenuDemo';
 
 const Header = () => {
     const [auth, setAuth] = useRecoilState(authState);
-    const [isDropdownOpen, setDropdownOpen] = useState(false);
+    const { data: session, status } = useSession();
 
-    const { data: session, status } = useSession({
-        required: false,
-        onUnauthenticated() {
-            // The user is not authenticated, handle it here
-            setAuth(null);
-        },
-    });
-
-    const toggleDropdown = () => {
-        if (status === 'unauthenticated') {
-            signIn('google', { callbackUrl: '/' })
-        }
-        else { setDropdownOpen(!isDropdownOpen) }
-    }
     // Sync session state with recoil state
     useEffect(() => {
         if (session) {
@@ -30,15 +16,10 @@ const Header = () => {
         }
     }, [session, setAuth]);
 
-    // const handleSignIn = () => {
-    //     if (status !== 'authenticated') {
-    //         signIn('google'); // Specify 'google' if it's the only provider
-    //     }
-    // };
     return (
         <>
             <div className='fixed w-screen top-0'>
-                <div className=' items-center px-4 py-4 md:px-8 md:py-4 xl:px-10 text-white flex justify-between'>
+                <div className='items-center px-4 py-4 md:px-8 md:py-4 xl:px-10 text-white flex justify-between'>
                     <div className="logo text-gray-400 text-sm md:text-lg xl:text-xl">
                         <Link href={'/'}>
                             Minimalist <span className='text-white'>Builder</span>
@@ -46,21 +27,7 @@ const Header = () => {
                     </div>
                     <div className='flex gap-2 justify-center items-center md:gap-4'>
                         <a href="https://twitter.com/imlastdeveloper" target='_blank'><i className="fa-brands fa-x-twitter scale-75 md:scale-100 xl:scale-110"></i></a>
-                        {/* <svg /> */}
-
-                        <svg width="15" onClick={toggleDropdown} className='md:scale-125 xl:scale-150 cursor-pointer' height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.877014 7.49988C0.877014 3.84219 3.84216 0.877045 7.49985 0.877045C11.1575 0.877045 14.1227 3.84219 14.1227 7.49988C14.1227 11.1575 11.1575 14.1227 7.49985 14.1227C3.84216 14.1227 0.877014 11.1575 0.877014 7.49988ZM7.49985 1.82704C4.36683 1.82704 1.82701 4.36686 1.82701 7.49988C1.82701 8.97196 2.38774 10.3131 3.30727 11.3213C4.19074 9.94119 5.73818 9.02499 7.50023 9.02499C9.26206 9.02499 10.8093 9.94097 11.6929 11.3208C12.6121 10.3127 13.1727 8.97172 13.1727 7.49988C13.1727 4.36686 10.6328 1.82704 7.49985 1.82704ZM10.9818 11.9787C10.2839 10.7795 8.9857 9.97499 7.50023 9.97499C6.01458 9.97499 4.71624 10.7797 4.01845 11.9791C4.97952 12.7272 6.18765 13.1727 7.49985 13.1727C8.81227 13.1727 10.0206 12.727 10.9818 11.9787ZM5.14999 6.50487C5.14999 5.207 6.20212 4.15487 7.49999 4.15487C8.79786 4.15487 9.84999 5.207 9.84999 6.50487C9.84999 7.80274 8.79786 8.85487 7.49999 8.85487C6.20212 8.85487 5.14999 7.80274 5.14999 6.50487ZM7.49999 5.10487C6.72679 5.10487 6.09999 5.73167 6.09999 6.50487C6.09999 7.27807 6.72679 7.90487 7.49999 7.90487C8.27319 7.90487 8.89999 7.27807 8.89999 6.50487C8.89999 5.73167 8.27319 5.10487 7.49999 5.10487Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
-                        {isDropdownOpen && (
-                            <div >
-                                <DropdownMenu>
-
-                                    <DropdownMenuContent>
-                                        <DropdownMenuItem onSelect={() => console.log('New webapp')}>New webapp</DropdownMenuItem>
-                                        {/* ... other dropdown items ... */}
-                                        <DropdownMenuItem onSelect={() => signIn('google')}>Sign out</DropdownMenuItem>
-                                    </DropdownMenuContent>
-                                </DropdownMenu>
-                            </div>
-                        )}
+                        {status==='authenticated'? <DropdownMenuDemo />: <svg width="15" onClick={()=>{signIn('google', { callbackUrl: '/' })}} className='md:scale-125 xl:scale-150 cursor-pointer' height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.877014 7.49988C0.877014 3.84219 3.84216 0.877045 7.49985 0.877045C11.1575 0.877045 14.1227 3.84219 14.1227 7.49988C14.1227 11.1575 11.1575 14.1227 7.49985 14.1227C3.84216 14.1227 0.877014 11.1575 0.877014 7.49988ZM7.49985 1.82704C4.36683 1.82704 1.82701 4.36686 1.82701 7.49988C1.82701 8.97196 2.38774 10.3131 3.30727 11.3213C4.19074 9.94119 5.73818 9.02499 7.50023 9.02499C9.26206 9.02499 10.8093 9.94097 11.6929 11.3208C12.6121 10.3127 13.1727 8.97172 13.1727 7.49988C13.1727 4.36686 10.6328 1.82704 7.49985 1.82704ZM10.9818 11.9787C10.2839 10.7795 8.9857 9.97499 7.50023 9.97499C6.01458 9.97499 4.71624 10.7797 4.01845 11.9791C4.97952 12.7272 6.18765 13.1727 7.49985 13.1727C8.81227 13.1727 10.0206 12.727 10.9818 11.9787ZM5.14999 6.50487C5.14999 5.207 6.20212 4.15487 7.49999 4.15487C8.79786 4.15487 9.84999 5.207 9.84999 6.50487C9.84999 7.80274 8.79786 8.85487 7.49999 8.85487C6.20212 8.85487 5.14999 7.80274 5.14999 6.50487ZM7.49999 5.10487C6.72679 5.10487 6.09999 5.73167 6.09999 6.50487C6.09999 7.27807 6.72679 7.90487 7.49999 7.90487C8.27319 7.90487 8.89999 7.27807 8.89999 6.50487C8.89999 5.73167 8.27319 5.10487 7.49999 5.10487Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>}
                     </div>
                 </div>
                 <hr className=" border-0 w-11/12 mx-auto" style={{ background: 'linear-gradient(to right, transparent, gray, transparent)', height: '.8px' }} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
+import { authState } from "@/state/authState";
+import { signIn } from "next-auth/react";
 import { Inter } from "next/font/google";
+import { useRouter } from "next/router";
+import { useRecoilValue } from "recoil";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const auth = useRecoilValue(authState);
+  const router = useRouter();
+
+  const handleStartNowClick = () => {
+    if (auth) {
+      router.push('/canvas');
+    } else {
+      signIn('google');
+    }
+  };
+
   return (
     <main className={`text-white ${inter.className} flex flex-col items-center justify-center h-screen `}>
       <div className="glow-effect text-2xl uppercase font-bold sm:text-3xl md:text-4xl xl:text-5xl">
@@ -9,7 +24,7 @@ export default function Home() {
         <p>Maximal Impact</p>
       </div>
       <p className="text-xs my-4 text-gray-400 sm:text-sm md:text-base xl:text-lg">The new age builder with <span className="text-white font-bold">AI</span> inside.</p>
-      <button className="button1 flex items-center gap-1 mt-2 px-4 py-2 border border-white text-sm  md:text-base xl:text-lg font-medium rounded-md text-black bg-white hover:bg-transparent hover:text-white">
+      <button onClick={handleStartNowClick} className="button1 flex items-center gap-1 mt-2 px-4 py-2 border border-white text-sm  md:text-base xl:text-lg font-medium rounded-md text-black bg-white hover:bg-transparent hover:text-white">
         Start Now
         <svg className="arrow-move" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
       </button>
